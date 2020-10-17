128716740a34f9ebdb7a7b83945ce2f0b04b860b
pdpurdue <pdantu@purdue.edu>
2020-10-17 12:18:20 -0400

    Created a Food Activity

--- app/src/main/java/com/example/yeldue/Food.java
+++ app/src/main/java/com/example/yeldue/Food.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class Food extends AppCompatActivity {
+    ArrayList<>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
